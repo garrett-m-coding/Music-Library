c1f9c243a91b8a29c65a1cf0d5f204e7fc59c9a9
garrett-m-coding <garrett.mcginnis.coding@gmail.com>
2021-10-12 00:37:25 -0500

    Added the pip instal django cors headers and changed setting python file to include cors hors headers.

--- music_library_gm_coding/music/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Song(models.Model):
-    title = models.CharField(max_length=100)
-    artist = models.CharField(max_length=100)
-    album = models.CharField(max_length=100)
-    release_date = models.DateTimeField()
-    genre = models.CharField(max_length=100)
--- music_library_gm_coding/music/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from music import views
-
-urlpatterns = [
-    path('music/', views.SongList.as_view()),
-    path('music/<int:pk>/', views.SongDetail.as_view()),
-]
